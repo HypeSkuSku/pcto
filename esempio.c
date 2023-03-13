01d159913ed33c468b019fa724973bbcd98b6800
HypeSkuSku <127745135+HypeSkuSku@users.noreply.github.com>
2023-03-13 15:45:50 +0100

    Add files via upload

---
+++ esempio.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+
+int main()
+{
+    int    numero = 42;
+    double numerolungo = 231365416543654;
+    float  numeroconlavirgola = 5.8936;
+    char   carattere = 'c';
+    
+    printf("il numero è: %d\n", numero);
+    printf("ïl numerolungo è: %f, il numero con la virgola è %f\n", numerolungo, numeroconlavirgola);
+    printf("ïl carattere è: %c\n", carattere);
+}
+    
+
